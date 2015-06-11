3cfac828d20e86512fcddfdda97891549fa7fcac
rizvi <rizvi22222@yahoo.com>
2015-06-11 13:37:18 +0600

    demo

--- web/index.php
+++ web/index.php
@@ -5,19 +5,6 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  
-  echo "shamir Ahmed";
-  
-});
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__.'/../views',
