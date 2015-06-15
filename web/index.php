b1fccdb160e6909aed4c9541e350da1f897afc02
rizvi <rizvi22222@yahoo.com>
2015-06-16 01:03:24 +0600

    jj

--- web/index.php
+++ web/index.php
@@ -160,12 +160,67 @@ $app->get('/qa', function() use($app) {
 			$result = curl_exec($ch);
 		
 		//$json = json_decode($result, true);
-		if(!$result || strlen(trim($result)) == 0)
+		if(strlen(trim($result))<=290)
 		{
-			return 'Empty Result';
-		}
+			
+			$string=$q;
+		
+			  $stopWords = array('i','a','about','an','and','are','as','at','be','by','com','de','en','for','from','how','in','is','it','la','of','on','or','that','the','this','to','was','what','when','where','who','will','with','und','the','www');
+		   
+			  $string = preg_replace('/\s\s+/i', '', $string);
+			  $string = trim($string);
+			  $string = preg_replace('/[^a-zA-Z0-9 -]/', '', $string);
+			  $string = strtolower($string);
+		   
+			  preg_match_all('/\b.*?\b/i', $string, $matchWords);
+			  $matchWords = $matchWords[0];
+			  
+			  foreach ( $matchWords as $key=>$item ) {
+				  if ( $item == '' || in_array(strtolower($item), $stopWords) || strlen($item) <= 3 ) {
+					  unset($matchWords[$key]);
+				  }
+			  }   
+			  $wordCountArr = array();
+			  if ( is_array($matchWords) ) 
+			  {
+				  foreach ( $matchWords as $key => $val ) 
+				  {
+					  $val = strtolower($val);
+					  if ( isset($wordCountArr[$val]) ) 
+					  {
+						  $wordCountArr[$val]++;
+					  } else 
+					  {
+						  $wordCountArr[$val] = 1;
+					  }
+				  }
+			  }
+			  $words= implode(',', array_keys($wordCountArr));
+			  $words = str_replace(',', ' ', $words);
 
-		return strlen(trim($result));
+			
+			  $textapi = new AYLIEN\TextAPI("098443aa", "e288284a802322d954b42740d0dfa95b");
+			  $url='https://en.wikipedia.org/wiki/'.$words;
+			  $summary= $textapi->summarize(array("url" => $url));
+			  $ans="";
+			  if($summary->text == null) 
+			  {
+				  $ans="Your majesty! Jon Snow knows nothing! So do I! Your grace, I searched a lot. But could not find a good free API. But I can manage Aylien API. By which I can show you information from Wiki Page. You can write only the topic name and information from the wiki page will be appeared. Enjoy, Kitty!";
+			  }else
+			  {
+				  $ans=$summary->text;
+			  }
+
+		}
+		else $ans=trim($result);
+		
+		$myarr = array(
+			'answer'  => $ans
+		);
+  
+		$js=json_encode($myarr);
+  
+		return $js;
 
 });
   
