7705190d2b41861696d55b87cc080358d8dae514
rizvi <rizvi22222@yahoo.com>
2015-06-15 11:57:32 +0600

    test

--- web/index.php
+++ web/index.php
@@ -16,7 +16,34 @@ $app->get('/greetings', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   
   $q=$_GET['q'];
-  return $q."moja";
+  $arr = explode('!', $q);
+  
+  $ans="";
+  if($arr[0]=="Hi")
+  {
+	$ans="I am fine.How are you?";
+  }
+  else if($arr[0]=="Hello")
+  {
+	$ans="My name is Shamir.";
+  }
+  else if($arr[0]=="Good morning" || $arr[0]=="Good evening" || arr[0]=="Good night")
+  {
+	$ans=$arr[0]."!I am Shamir! I am also pleased to meet you!";
+  }
+  else
+  {
+	$ans="Sorry, I don't understand your question."
+  }
+  
+  $ans="Hello, Kitty!".$ans;
+  
+  $myarr = array(
+  'answer'  => $ans
+  );
+  
+  
+  return json_encode($myarr);
   
 });
 
